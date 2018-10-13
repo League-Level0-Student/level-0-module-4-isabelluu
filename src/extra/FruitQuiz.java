87d86122ab4760ae7aa1a361ef0212653772915a
11000783 <josephaguilar2022@gmail.com>
2018-10-13 14:29:15 -0700

    Day 7

--- src/extra/FruitQuiz.java
+++ src/extra/FruitQuiz.java
@@ -16,15 +16,18 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
+		question2 = new JLabel("<html>Which is not a real animal? <br> A: Narwol <br> B: Platypus  <br> C: Unicorn</html>");
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		// 1. Print out the key code variable
-
+		System.out.println(keyCode);
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
-		
+		int A = 65 & 16;
+		int b =66 & 16;
+		int C =67 & 16;
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
 		
 		// 12. If question2 is showing,
@@ -34,16 +37,17 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			
+			if(equals(C))
 				// 4. Call the correct() method
-				
+				correct();
 				// 7. Use the nextQuestion() method to go to question2
-			
+			nextQuestion(question2);
 			
 			// 8. else (if they touched something else)
-				
+			//else {
 				// 9. Call the incorrect() method
-		
+				incorrect();
+			//}
 		}
 
 	}
